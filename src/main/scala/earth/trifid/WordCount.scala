af1b155f98aaacf300f0425eedee4266108d7cd2
stevenwernercs <stevenwernercs.gmail.com>
2018-10-17 02:30:01 -0500

    working test example

--- src/main/scala/earth/trifid/WordCount.scala
+++ src/main/scala/earth/trifid/WordCount.scala
@@ -6,6 +6,7 @@ import org.apache.spark.SparkConf
 object WordCounter {
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("Word Counter")
+      //conf.setMaster("local");
     val sc = new SparkContext(conf)
     val textFile = sc.textFile("file:///shared/Macky/disk1/work/iSteve/projects/spark-test/test.txt")
     val tokenizedFileData = textFile.flatMap(line=>line.split(" "))
